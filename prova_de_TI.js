871bd0ee0eabb121b3999c8d8e815c5b321a8793
arthur972645 <arthurall.1973@gmail.com>
2023-07-27 16:14:35 -0300

    Adicionado prova de Ti

---
+++ prova_de_TI.js
@@ -0,0 +1,33 @@
+const nota1_1 = Number(prompt("Digite sua nota: "));
+const nota2_1 = Number(prompt("Digite sua nota: "));
+const nota3_1 = Number(prompt("Digite sua nota: "));
+const nota4_1 = Number(prompt("Digite sua nota: "));
+
+const mediaA1 = (nota1_1 + nota2_1 + nota3_1 + nota4_1) / 4 .toFixed(2)
+
+const nota1_2 = Number(prompt("Digite sua nota: "));
+const nota2_2 = Number(prompt("Digite sua nota: "));
+const nota3_2 = Number(prompt("Digite sua nota: "));
+const nota4_2 = Number(prompt("Digite sua nota: "));
+
+const mediaA2 = (nota1_2 + nota2_2 + nota3_2 + nota4_2) / 4 .toFixed(2)
+
+const nota1_3 = Number(prompt("Digite sua nota: "));
+const nota2_3 = Number(prompt("Digite sua nota: "));
+const nota3_3 = Number(prompt("Digite sua nota: "));
+const nota4_3 = Number(prompt("Digite sua nota: "));
+
+const mediaA3 = (nota1_3 + nota2_3 + nota3_3 + nota4_3) / 4 .toFixed(2)
+
+const nota1_4 = Number(prompt("Digite sua nota: "));
+const nota2_4 = Number(prompt("Digite sua nota: "));
+const nota3_4 = Number(prompt("Digite sua nota: "));
+const nota4_4 = Number(prompt("Digite sua nota: "));
+
+const mediaA4 = (nota1_4 + nota2_4 + nota3_4 + nota4_4) / 4 .toFixed(2)
+
+
+
+
+const mediaGeral = (mediaA1 + mediaA2 + mediaA3 + mediaA4) / 4;
+alert(`MediaA1 = ${mediaA1} \n MediaA2 =${mediaA2} \n MediaA3= ${mediaA3} \n MediaA4= ${mediaA4} \n Media geral =${mediaGeral}`)
